69ab718010cb60ab546d48df1faea61fd5b8fa38
Sam <sam@awkwardengineer.com>
2014-01-05 22:33:08 -0500

    added calibration and set time modes to switch selector

--- Clock/Clock.ino
+++ Clock/Clock.ino
@@ -2,9 +2,20 @@
 #define HOURSPIN 0
 #define MINUTESPIN 4
 #define KNOBPIN 0  // read analog port 0, which on the digispark, is P5
+#define SWITCHPIN 1  // read analog port 1, which on the digispark is P2
 
 #define SF_SECS .2553
 #define SF_TENSECS .0255
+
+#define SF_HOURS 43200000L
+#define SF_MINS 3600000L
+
+#define BAND1 85
+#define BAND2 256
+#define BAND3 426
+#define BAND4 597
+#define BAND5 768
+#define BAND6 938
 
 //analog port 0 = pin5
 //analog port 1 = pin2
@@ -12,13 +23,19 @@
 //analog port 3 = pin3
 
 
-
+//<<<   inputs   >>>
 int valKnob=0;
 int valSwitch=0;
+
+//<<<   outputs   >>>
 int hValDisplay=0;
 int mValDisplay=0;
 
+//<<<    internal   >>>
 long milliStart;
+long time = 0;
+int maxHRange = 240;
+int maxMRange = 240;
 
 
 // the setup routine runs once when you press reset:
@@ -31,15 +48,61 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
   
-  valKnob = analogRead(KNOBPIN); //divide by 4 because read is 0-1023, while write is 0-255
+  valKnob = analogRead(KNOBPIN); //read is 0-1023, while write is 0-255
+  valSwitch = analogRead(SWITCHPIN);
   
-  if (true){ // this will eventually be a series of if statements for each Switch State
+  if (valSwitch > BAND6){ // this will eventually be a series of if statements for each Switch State
     // this case is the "test mode" which should scroll through the display very quickly.
     
       milliStart = millis();
-      hValDisplay = int(float(milliStart % 10000) * SF_TENSECS);
-      mValDisplay = int(float(milliStart % 1000) * SF_SECS);
+      hValDisplay = int(float(milliStart % 10000)/10000. * float(maxHRange) );
+      mValDisplay = int(float(milliStart % 1000)/1000. * float(maxMRange) );
        
+  }
+  else if (BAND5 < valSwitch && valSwitch <= BAND6){
+    // this mode is to calibate the maximum hours output
+    if (valKnob > BAND4 && maxHRange < 255){
+      maxHRange = maxHRange + 1;
+    }
+    else if (valKnob < BAND2 && maxHRange > 0){
+      maxHRange = maxHRange - 1;
+    }
+    
+    hValDisplay = maxHRange;
+    mValDisplay = 0; 
+    delay(100);   
+  }
+  else if (BAND4 < valSwitch && valSwitch <= BAND5){
+    // this mode is to calibate the maximum minutes output
+    if (valKnob > BAND4 && maxMRange < 255){
+      maxMRange = maxMRange + 1;
+    }
+    else if (valKnob < BAND2 && maxHRange > 0){
+      maxMRange = maxMRange - 1;
+    }
+
+    hValDisplay = 0;
+    mValDisplay = maxMRange;
+    delay(100);   
+  }
+  else if (valSwitch <= BAND4){
+    if (valKnob > BAND4){
+      time = time + 60000; //1 min = 60 sec * 1000 ms
+    }
+    else if (valKnob < BAND2 && maxHRange > 0){
+      time = time - 60000; //1 min = 60 sec * 1000 ms
+    }
+    
+    delay(100);   
+
+    milliStart = millis() + time;
+    
+    hValDisplay = int(float(milliStart % SF_HOURS)/43200000. * float(maxHRange) );
+    mValDisplay = int(float(milliStart % SF_MINS)/3600000. * float(maxMRange) );
+  }
+  else{
+    hValDisplay = 0;
+    mValDisplay = 0; 
   }
     
     
